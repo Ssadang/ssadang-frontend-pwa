3a205852aeb64a0151b81bef54fbe03301cf2009
도경록 <dogyeonglog@dogyeonglogs-MacBook-Air.local>
2024-12-18 15:03:20 +0900

    Feat: Auth 관련 SignInPage Router 연결 기능 구현

---
+++ src/pages/AuthPage/AuthSignInPage/AuthSignInPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function AuthSignInPage() {
+  return (
+    <div>AuthSignInPage</div>
+  )
+}
+
+export default AuthSignInPage;
--- src/routes/AuthRoute/AuthRoute.jsx
+++ src/routes/AuthRoute/AuthRoute.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import AuthMainPage from '../../pages/AuthPage/AuthMainPage/AuthMainPage';
+import AuthSignInPage from '../../pages/AuthPage/AuthSignInPage/AuthSignInPage';
 
 function AuthRoute() {
   return (
     <>
         <Routes>
             <Route path='/auth' element={ <AuthMainPage /> } />
+            <Route path='/auth/signin' element={ <AuthSignInPage /> } />
         </Routes>
     </>
   )
