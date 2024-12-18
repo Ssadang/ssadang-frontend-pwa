e450aec93aba64e839354bc0c6662f040c0719be
도경록 <dogyeonglog@dogyeonglogs-MacBook-Air.local>
2024-12-18 23:18:29 +0900

    Design: Auth 관련 회원정보 찾기 페이지 작업 완료

--- src/pages/AuthPage/AuthFindUserInfoPage/style.js
+++ src/pages/AuthPage/AuthFindUserInfoPage/style.js
@@ -1,1 +1,9 @@
 import styled from "styled-components";
+
+export const RevertContainer = styled.div`
+    box-sizing: border-box;
+    padding: 10px;
+    display: flex;
+    align-items: center;
+    height: 50px;
+`;
