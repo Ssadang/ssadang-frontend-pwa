2b547e9e2fba753ec980e909510b4374a760742a
도경록 <dogyeonglog@dogyeonglogs-MacBook-Air.local>
2024-12-19 23:37:13 +0900

    Feat: Auth 관련 회원가입 페이지 Router 연결 구현

--- src/pages/AuthPage/AuthSignUpPage/AuthSignUpPage.jsx
+++ src/pages/AuthPage/AuthSignUpPage/AuthSignUpPage.jsx
@@ -1,8 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { RevertContainer, SignUpInputContainer } from './style';
+import { FaChevronLeft } from "react-icons/fa";
+import { useNavigate } from 'react-router-dom';
+import SignUpComponent from '../../../components/SignUpComponent/SignUpComponent';
 
 function AuthSignUpPage() {
+  const navigate = useNavigate();
+
+  // 회원가입 1
+  const [ email, setEmail ] = useState('');
+  const [ password, setPassword ] = useState('');
+  const [ passwordCheck, setPasswordCheck ] = useState('');
+  const [ name, setName ] = useState('');
+  const [ nickname, setNickname ] = useState('');
+  const [ location, setLocation ] = useState(null); // select
+  const [ semester, setSemester ] = useState(null); // select
+
+  // 회원가입 2
+  const [ emailAuthCode, setEmailAuthCode ] = useState('');
+  const [ authImg, setAuthImg ] = useState(null);
+  
+
+  const goBackButtonOnClickHandler = () => {
+    navigate(-1);
+  }
+
   return (
-    <div>AuthSignUpPage</div>
+    <>
+      <RevertContainer>
+        <FaChevronLeft onClick={goBackButtonOnClickHandler} />
+      </RevertContainer>
+      <SignUpInputContainer>
+        <SignUpComponent
+          email={email}
+          setEmail={setEmail}
+          password={password}
+          setPassword={setPassword}
+          passwordCheck={passwordCheck}
+          setPasswordCheck={setPasswordCheck}
+          name={name}
+          setName={setName}
+          nickname={nickname}
+          setNickname={setNickname}
+          location={location}
+          setLocation={setLocation}
+          semester={semester}
+          setSemester={setSemester}
+          emailAuthCode={emailAuthCode}
+          setEmailAuthCode={setEmailAuthCode}
+          authImg={authImg}
+          setAuthImg={setAuthImg}
+        />
+      </SignUpInputContainer>
+    </>
   )
 }
 
